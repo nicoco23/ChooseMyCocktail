6ff913f2f8e050b082598f83fdaf48d2e27a12b2
nicoco23 <nicolas.tassin23@gmail.com>
2024-04-24 02:21:12 +0200

    FIX : display cocktailPage

--- choose-my-cocktail/src/pages/AllCocktailPage.jsx
+++ choose-my-cocktail/src/pages/AllCocktailPage.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Ingredient from '../components/Ingredients';
 
-function AllCocktailsPage({ cocktails }) {
-  console.log(cocktails);
+function AllCocktailsPage() {
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    fetch('/path/to/your/cocktails/data.json') // Remplacez par le chemin vers vos données de cocktails
+      .then(response => response.json())
+      .then(data => setCocktails(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div style={{ padding: '20px' }}>
       <h1 style={{ color: '#007bff', fontSize: '2.5em', textAlign: 'center' }}>Tous les Cocktails</h1>
       {/* Afficher les cocktails */}
-      {Array.isArray(cocktails) && cocktails.map(cocktail => (
+      {cocktails.map(cocktail => (
         <div key={cocktail.nom} style={{ marginBottom: '20px', border: '1px solid #ccc', borderRadius: '10px', padding: '10px' }}>
           <h2 style={{ color: '#333', fontSize: '2em' }}>{cocktail.nom}</h2>
           <p style={{ fontSize: '1.2em', lineHeight: '1.6' }}>{cocktail.description}</p>
