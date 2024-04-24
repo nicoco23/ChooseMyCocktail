eaa7420d491ca27f51fb3ddf2097bf8ead841084
nicoco23 <nicolas.tassin23@gmail.com>
2024-04-24 05:10:25 +0200

    Déco

--- choose-my-cocktail/src/pages/HomePage.jsx
+++ choose-my-cocktail/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
+    <div style={{ padding: '50px', textAlign: 'center' }}>
       <h1 style={{ color: '#007bff', fontSize: '2.5em' }}>Bienvenue sur notre site de cocktails !</h1>
       <p style={{ fontSize: '1.2em', lineHeight: '1.6' }}>Découvrez une sélection de délicieux cocktails et apprenez à les préparer vous-même. Que vous soyez un amateur de cocktails ou un barman professionnel, vous trouverez ici des recettes pour satisfaire votre soif de découverte et de créativité.</p>
     </div>
